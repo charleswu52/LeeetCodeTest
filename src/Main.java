09e7f34ee4fd9e7a97aa39faeaca47d4fd85b381
charleswu52 <charleswu5201@gmail.com>
2021-01-27 00:36:22 +0800

    每日打卡--2021/1/27

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import com.leetcodetest.Jan.*;
 public class Main {
     public static void main(String[] args) {
-        _26 test = new _26();
-        test._21_1_26();
+        _27 test = new _27();
+        test._21_1_27();
     }
 }
 
