df74bd5d6e5f44f6fd79fe4a838493addbb079f1
charleswu52 <charleswu5201@gmail.com>
2021-02-17 16:56:20 +0800

    每日一题--2021/2/1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import com.leetcodetest.Feb.*;
 public class Main {
     public static void main(String[] args) {
-        _16 test = new _16();
-        test._21_2_16();
+        _17 test = new _17();
+        test._21_2_17();
 
     }
 }
