9f68be76a96a4b1d6dc9265434f7aa34f17effcb
charleswu52 <charleswu5201@gmail.com>
2021-06-27 11:25:55 +0800

    LeetCode每日一题--2021/6/27

--- LeetCode/src/leetcodetest/Jun/_26.java
+++ LeetCode/src/leetcodetest/Jun/_26.java
@@ -48,9 +48,9 @@ public class _26 {
     为了方便记录和搜索，要将状态保存在一个hash表中，但是hashMap 的存数组的话是没法取到其value的，因此可以将其转化为一个String进行存储。
     同时假设0存在每个位置上，那它可以进行交换的位置也都存起来，在搜索的时候进行
      */
-
     // 为了方便进行搜索，将二维数组每个位置可以交换的位置进行记录
-    public  static int[][] neighbors = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+    public static int[][] neighbors = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+
     public int slidingPuzzle(int[][] board) {
         // 将初始状态转为 String 进行存储
         StringBuilder stringBuilder = new StringBuilder();
@@ -73,19 +73,18 @@ public class _26 {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String state = queue.poll();
-               for (String cur:changeStae(state)){
-                   if (!visited.contains(cur)) {
-                       if ("123450".equals(cur)) {
-                           return res;
-                       }
-                       queue.offer(cur);
-                       visited.add(cur);
-                   }
-               }
+                for (String cur : changeStae(state)) {
+                    if (!visited.contains(cur)) {
+                        if ("123450".equals(cur)) {
+                            return res;
+                        }
+                        queue.offer(cur);
+                        visited.add(cur);
+                    }
+                }
             }
         }
         return -1;
-
 
 
     }
@@ -107,7 +106,6 @@ public class _26 {
         chars[x] = chars[y];
         chars[y] = temp;
     }
-
 
 
     @Test
