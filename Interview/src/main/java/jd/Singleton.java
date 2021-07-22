831753817c362953c707a9067640a66de2008ef6
charleswu52 <charleswu5201@gmail.com>
2021-07-22 09:06:57 +0800

    leetcode everyday 2021/07/21 interview jd

---
+++ Interview/src/main/java/jd/Singleton.java
@@ -0,0 +1,23 @@
+package jd;
+
+/**
+ * @author WuChao
+ * @create 2021/7/21 18:17
+ */
+public class Singleton {
+    private volatile static Singleton singleton;
+    private Singleton() {
+
+    }
+
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
