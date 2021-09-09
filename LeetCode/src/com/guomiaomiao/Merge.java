e18dd66b6cae9c6ef571e846b912ee2f3f4a7ad8
charleswu52 <charleswu5201@gmail.com>
2021-09-09 15:49:40 +0800

    leetcode everyday 2021/9/9

---
+++ LeetCode/src/com/guomiaomiao/Merge.java
@@ -0,0 +1,65 @@
+package com.guomiaomiao;
+
+/**
+ * @author WuChao
+ * @create 2021/9/9 14:16
+ */
+
+/**
+ * 链表节点定义
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Merge {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(5);
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode res = mergeTwoLists(node1, node4);
+
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+
+    }
+
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
--- LeetCode/src/leetcode_everyday/Sep/_9_68.java
+++ LeetCode/src/leetcode_everyday/Sep/_9_68.java
@@ -70,8 +70,84 @@ public class _9_68 {
      */
 
 
-    public List<String> fullJustify(String[] words, int maxWidth) {
-        return new ArrayList<>();
+    /*
+    思路：贪心思想
+    对于每一行，我们首先确定最多可以放置多少单词，这样可以得到该行的空格个数，从而确定该行单词之间的空格个数。
+    对于每一行填充细节，分三种情况讨论：
+        1.当前行是最后一行：单词左对齐，且单词之间应只有一个空格，在行末填充剩余空格；
+        2.当前行不是最后一行，且只有一个单词：该单词左对齐，在行末填充空格；
+        3.当前行不是最后一行，且不只一个单词：设当前行单词数为 numWords，空格数为 numSpaces，我们需要将空格均匀分配在单词之间，
+            则单词之间应至少有avgSpaces=⌊ numSpaces / (numWords−1) ⌋ 个空格，对于多出来的
+            extraSpaces=numSpaces mod (numWords−1) 个空格，应填在前 extraSpaces 个单词之间。因此，前 extraSpaces 个单词之间填充
+            avgSpaces+1 个空格，其余单词之间填充 avgSpaces 个空格。
+     */
 
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> ans = new ArrayList<>();
+        int right = 0, n = words.length;
+        while (true) {
+            int left = right;// 当前行的第一个单词在 words 的位置
+            int sumLen = 0; // 统计这一行单词长度之和
+            // 循环确定当前行可以放多少单词，注意单词之间应至少有一个空格
+            while (right < n && sumLen + words[right].length() + right - left <= maxWidth) {
+                sumLen += words[right++].length();
+            }
+            // 当前行是最后一行：单词左对齐，且单词之间应只有一个空格，在行末填充剩余空格
+            if (right == n) {
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append(blank(maxWidth - stringBuffer.length()));
+                ans.add(stringBuffer.toString());
+                return ans;
+            }
+
+            int numWords = right - left;
+            int numSpaces = maxWidth - sumLen;
+            // 当前行只有一个单词：该单词左对齐，在行末填充剩余空格
+            if (numWords == 1) {
+                StringBuffer stringBuffer = new StringBuffer(words[left]);
+                stringBuffer.append(numSpaces);
+                ans.add(stringBuffer.toString());
+                continue;
+            }
+            // 当前行不只一个单词
+            int avgSpaces = numSpaces / (numWords - 1);
+            int extraSpaces = numSpaces % (numWords - 1);
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append(join(words, left, left + extraSpaces + 1, blank(avgSpaces + 1)));// 拼接额外加一个空格的单词
+            stringBuffer.append(blank(avgSpaces));
+            stringBuffer.append(join(words, left + extraSpaces + 1, right, blank(avgSpaces))); // 拼接其余单词
+            ans.add(stringBuffer.toString());
+        }
     }
+
+    /**
+     * blank 返回长度为 n 的由空格组成的字符串
+     * @param n
+     * @return
+     */
+    public String blank(int n) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; ++i) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    /**
+     * join 返回用 sep 拼接 [left, right) 范围内的 words 组成的字符串
+     * @param words
+     * @param left
+     * @param right
+     * @param sep
+     * @return
+     */
+    public StringBuffer join(String[] words, int left, int right, String sep) {
+        StringBuffer sb = new StringBuffer(words[left]);
+        for (int i = left + 1; i < right; ++i) {
+            sb.append(sep);
+            sb.append(words[i]);
+        }
+        return sb;
+    }
+
 }
