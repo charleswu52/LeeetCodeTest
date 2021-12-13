f55b99d084cc32aeedf2f2349049ac71661d7f48
charleswu52 <charleswu5201@gmail.com>
2021-12-13 10:38:24 +0800

    LeetCode everyday --2021/12/13 807. 保持城市天际线

---
+++ ESTest/src/main/java/logparser/SplitJson.java
@@ -0,0 +1,68 @@
+package logparser;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author WuChao
+ * @create 2021/12/12 下午3:46
+ */
+public class SplitJson {
+    public static void main(String[] args)throws Exception {
+        // 读取 InfluxDB json 日志 并按照 schema 进行划分
+        String filePath = "/media/charles/My Passport/Work/CompressData/exampleData/influxdb-2021-03-30T11-06-23.613.log"; // json 日志 文件
+        String outputPath = "/media/charles/My Passport/Work/CompressData/exampleData/influxdbOut/";
+        File file = new File(filePath);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(file), StandardCharsets.UTF_8));
+
+        String temp = null;
+        int id = 0; // 标记是几行数据
+        Set<String> stringSet = new HashSet<>();
+        while ((temp = reader.readLine()) != null) {
+            id++;
+            JSONObject jsonObject = JSONObject.parseObject(temp, Feature.OrderedField); // 按照 json 原先的顺序解析
+            Iterator<Map.Entry<String, Object>> iterator = jsonObject.entrySet().iterator();
+            StringBuilder fieldValues = new StringBuilder();
+            int idx = 0;
+            while (iterator.hasNext()) {
+                Map.Entry<String, Object> next = iterator.next();
+                String fieldVal = String.valueOf(next.getValue());
+                if (idx == 1) {
+                    SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.ENGLISH);
+                    fieldValues.append(date.parse(fieldVal).getTime()+" ");
+                }else if (idx == 2) {
+                    fieldValues.append(fieldVal + ": ");
+                } else {
+                    fieldValues.append(fieldVal + " ");
+                }
+                idx++;
+            }
+
+
+
+            File newFile = new File(outputPath + "influxdb2_test.log");
+            if (!newFile.exists()) {
+                newFile.createNewFile();
+            }
+            // 获取该文件的缓冲输出流
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(newFile, true), StandardCharsets.UTF_8));
+            // 写入信息
+            bufferedWriter.write(fieldValues + "\n");
+            bufferedWriter.flush();// 清空缓冲区
+            bufferedWriter.close();// 关闭输出流
+
+
+        }
+        reader.close();
+
+
+    }
+}
---
+++ LeetCode/src/leetcode_everyday/Dec/_13_807.java
@@ -0,0 +1,118 @@
+package leetcode_everyday.Dec;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author WuChao
+ * @create 2021/12/13 上午9:08
+ */
+public class _13_807 {
+    /**
+     * 每日一题：2021/12/13
+     * <p>
+     * 807. 保持城市天际线
+     * <p>
+     * 难度：medium
+     * <p>
+     * 在二维数组grid中，grid[i][j]代表位于某处的建筑物的高度。 我们被允许增加任何数量（不同建筑物的数量可能不同）的建筑物的高度。
+     * 高度 0 也被认为是建筑物。
+     *
+     * 最后，从新数组的所有四个方向（即顶部，底部，左侧和右侧）观看的“天际线”必须与原始数组的天际线相同。 城市的天际线是从远处观看时，
+     * 由所有建筑物形成的矩形的外部轮廓。 请看下面的例子。
+     *
+     * 建筑物高度可以增加的最大总和是多少？
+     *
+     * <p>
+     * 示例 1：
+     * <p>
+     * 例子：
+     * 输入： grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+     * 输出： 35
+     * 解释：
+     * The grid is:
+     * [ [3, 0, 8, 4],
+     *   [2, 4, 5, 7],
+     *   [9, 2, 6, 3],
+     *   [0, 3, 1, 0] ]
+     *
+     * 从数组竖直方向（即顶部，底部）看“天际线”是：[9, 4, 8, 7]
+     * 从水平水平方向（即左侧，右侧）看“天际线”是：[8, 7, 9, 3]
+     *
+     * 在不影响天际线的情况下对建筑物进行增高后，新数组如下：
+     *
+     * gridNew = [ [8, 4, 8, 7],
+     *             [7, 4, 7, 7],
+     *             [9, 4, 8, 7],
+     *             [3, 3, 3, 3] ]
+     *
+     * <p>
+     * 范围
+     * <p>
+     * 1 < grid.length = grid[0].length <= 50。
+     * grid[i][j] 的高度范围是： [0, 100]。
+     * 一座建筑物占据一个grid[i][j]：换言之，它们是 1 x 1 x grid[i][j] 的长方体。
+     */
+
+    /*
+    思路：贪心
+     */
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[] rows = new int[n];
+        int[] cols = new int[n];
+        for (int i = 0; i < n; i++) {
+            rows[i] = Arrays.stream(grid[i]).max().getAsInt();
+            cols[i] = grid[0][i];
+            for (int j = 0; j < n; j++) {
+                if (grid[j][i] > cols[i]) {
+                    cols[i] = grid[j][i] ;
+                }
+            }
+        }
+        int[][] newGrid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                newGrid[i][j] = Math.min(rows[i], cols[j]);
+            }
+        }
+        int oldSum = 0;
+        for (int[] arr : grid) {
+            oldSum += Arrays.stream(arr).sum();
+        }
+        int newSum = 0;
+        for (int[] arr : newGrid) {
+            newSum += Arrays.stream(arr).sum();
+        }
+        return newSum - oldSum;
+    }
+
+    /*
+    思路2：贪心优化
+     */
+    public int maxIncreaseKeepingSkyline2(int[][] grid) {
+        int n = grid.length;
+        int[] rows = new int[n];
+        int[] cols = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rows[i] = Math.max(rows[i], grid[i][j]);
+                cols[j] = Math.max(cols[j], grid[i][j]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += Math.min(rows[i], cols[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+
+        @Test
+    public void test() {
+        int[][] grid = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
+        System.out.println(maxIncreaseKeepingSkyline(grid));
+    }
+}
